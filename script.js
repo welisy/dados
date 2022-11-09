d2e3a2c8532dc03fc1e38b152debbaeaf51ad3ea
wesley <wesley>
2022-11-09 16:21:46 -0300

    Tipos de Dados

---
+++ script.js
@@ -0,0 +1,32 @@
+// Declare uma variável contendo uma string
+var nome = 'wesley'
+
+console.log(nome);
+
+
+// Declare uma variável contendo um número dentro de uma string
+var num = 'num 1';
+
+console.log(num);
+
+// Declare uma variável com a sua idade
+var idade = 19;
+
+console.log(`idade ${idade}`);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Diogo';
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto); 
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = "It's time"
+
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
---
+++ script2.js
@@ -0,0 +1,12 @@
+var nome = 'wesley';
+var idade = 19;
+
+console.log(typeof nome);
+
+
+//concatenando variavel com strings
+//ultilizando ( `${}` )
+var gols = 1000;
+var frase1 = `Romario fez ${gols} gols`;
+
+console.log(frase1);
